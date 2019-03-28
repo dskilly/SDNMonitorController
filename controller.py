cb7a2a0e03347a4681372b614b50760c0304e571
dskilly <nskilly@gmail.com>
2019-03-28 05:10:27 -0400

    Added code to utils for requesting other stats, queue, flow, desc and table, missing code to handle stats reply

--- controller.py
+++ controller.py
@@ -7,7 +7,7 @@ import psycopg2 as sql
 from datetime import datetime
 
 from .utils import db_handle
-from .switch import SwitchWrap
+from .switch import SwitchHandler
 from .discovery import topology_discovery
 from .settings import *
 
@@ -15,14 +15,12 @@ def launch(interval=5):
 	dh = db_handle(interval)
 	conn = sql.connect(db)
 	c = conn.cursor()
-	topo = {}
 	'''netjsongraph_topo_table = 'django_netjsongraph_topology'
 	c.execute("SELECT 1 FROM {} WHERE id = ?".format(netjsongraph_topo_table), (topo_name,))
 	if c.fetchone() is None:
 		c.execute("INSERT INTO {} (id, label, created, modified, url, protocol, version, revision, metric, published, strategy, expiration_time, key, parser) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)".format(netjsongraph_topo_table), (topo_name, topo_name, datetime.now(), datetime.now(), "", "", "", "", "", True, "Manual", -1, "", "NetJSON NetworkGraph"))
 		conn.commit()'''
-	switch = SwitchWrap(topo)
-	core.registerNew(SwitchWrap.SwitchHandler)
+	core.registerNew(SwitchHandler)
 	if 'openflow_discovery' not in core.components:
 		core.registerNew(Discovery)
 	core.registerNew(topology_discovery)
