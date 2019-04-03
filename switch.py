3cf86709b350891ad49361cc0443deabad90837f
dskilly <nskilly@gmail.com>
2019-04-03 07:42:26 -0400

    Added insert if not exist else do update for some stats handlers

--- switch.py
+++ switch.py
@@ -43,7 +43,7 @@ class SwitchHandler():
 		for f in event.stats:
 			self.received[sw] = self.received[sw] - f.rx_bytes
 			self.transmitted[sw] = self.transmitted[sw] - f.tx_bytes
-			c.execute('INSERT INTO "{}" (device_id, port_id, rx_packets, tx_packets, rx_dropped, tx_dropped, rx_errors, tx_errors) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)'.format(tables.ports), (f.device_id, f.port_id, f.rx_packets, f.tx_packets, f.rx_dropped, f.tx_dropped, f.rx_errors, f.tx_errors))
+			c.execute('INSERT INTO "{}" (device_id, port_id, rx_packets, tx_packets, rx_dropped, tx_dropped, rx_errors, tx_errors) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT (port_id) DO UPDATE SET rx_packets = %s, tx_packets = %s, rx_dropped = %s, tx_dropped = %s, rx_errors = %s, tx_errors = %s'.format(tables.ports), (f.device_id, f.port_no, f.rx_packets, f.tx_packets, f.rx_dropped, f.tx_dropped, f.rx_errors, f.tx_errors, f.rx_packets, f.tx_packets, f.rx_dropped, f.tx_dropped, f.rx_errors, f.tx_errors))
 			logger("Switch {} on port {} has received {} bytes and transmitted {} bytes.".format(sw, f.port_no, self.received, self.transmitted))
 		conn.commit()
 
@@ -63,7 +63,7 @@ class SwitchHandler():
 			self.max_entries[sw] = f.max_entries
 			self.tableActiveCount[sw] = f.active_count
 			logger("TableStatsReceived %s"%self.tableActiveCount)
-			c.execute('INSERT INTO "{}" (table_id, name, wildcards, max_entries, active_count, lookup_count, matched_count) VALUEs (%s, %s, %s, %s, %s, %s, %s)'.format(tables.flowtables), (f.table_id, f.name, f.wildcards, f.max_entries, f.active_count, f.lookup_count, f.matched_count))
+			c.execute('INSERT INTO "{}" (table_id, name, wildcards, max_entries, active_count, lookup_count, matched_count) VALUES (%s, %s, %s, %s, %s, %s, %s) ON CONFLICT (table_id) DO UPDATE SET active_count = %s, lookup_count = %s, matched_count = %s'.format(tables.flowtables), (f.table_id, f.name, f.wildcards, f.max_entries, f.active_count, f.lookup_count, f.matched_count, f.active_count, f.lookup_count, f.matched_count))
 			logger("Switch {} flow table {} with max entries {}, {} active flows and {} matched.".format(sw, f.table_id if not f.name else f.name, f.max_entries, f.active_count, f.matched_count))
 		conn.commit()
 
