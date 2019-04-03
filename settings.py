7956a23be2d2e7086353d0fe516c6c0cd8bf0dc2
dskilly <nskilly@gmail.com>
2019-04-03 07:10:35 -0400

    Done parts of stats handling, still needs testing

--- settings.py
+++ settings.py
@@ -3,3 +3,10 @@ from os import path
 db = 'dbname=sdnmonitorapp user=sdnmonitor host=localhost password=memeteam'
 
 topo_name = 'SDNMonitorTopoTest'
+
+class tables:
+	ports = 'Appmonitor_port_stats'
+	flows = 'Appmonitor_flow_stats'
+	descs = 'Appmonitor_desc_stats'
+	flowtables = 'Appmonitor_table_stats'
+	queues = 'Appmonitor_queue_stats'
--- switch.py
+++ switch.py
@@ -12,8 +12,8 @@ class SwitchHandler():
 	def __init__(self):
 		self.macs = {}
 		core.openflow.addListeners(self)
-		self.received = 0
-		self.transmitted = 0
+		self.received = {}
+		self.transmitted = {}
 		self.interval = 0.5
 
 	def _handle_ConnectionUp(self, event):
@@ -35,37 +35,47 @@ class SwitchHandler():
 		event.connection.send(msg)
 
 	def _handle_PortStatsReceived(self, event):
+		conn = sql.connect(db)
+		c = conn.cursor()
+		sw = event.dpid
 		for f in event.stats:
-			self.received = self.received - f.rx_bytes
-			self.transmitted = self.transmitted - f.tx_bytes
-		logger("Switch {} has received {} bytes and transmitted {} bytes.".format(event.dpid, self.received, self.transmitted))
+			self.received[sw] = self.receive[sw] - f.rx_bytes
+			self.transmitted[sw] = self.transmitted[sw] - f.tx_bytes
+			c.execute('INSERT INTO "{}" (device_id, port_id, rx_packets, tx_packets, rx_dropped, tx_dropped, rx_errors, tx_errors) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)'.format(tables.ports), (f.device_id, f.port_id, f.rx_packets, f.tx_packets, f.rx_dropped, f.tx_dropped, f.rx_errors, f.tx_errors))
+			logger("Switch {} on port {} has received {} bytes and transmitted {} bytes.".format(sw, f.port_no, self.received, self.transmitted))
+		conn.commit()
 
 	def _handle_FlowStatsReceived(self, event):
+		conn = sql.connect(db)
+		c = conn.cursor()
 		for f in event.stats:
-			pass
+			c.execute('INSERT INTO "{}" (length, table_id, duration_sec, priority, idle_timeout, hard_timeout, packet_count) VALUES (%s, %s, %s, %s, %s, %s, %s)'.format(tables.flows), (f.length, f.table_id, f.duration_sec, f.priority, f.idle_timeout, f.hard_timeout, f.packet_count))
+			logger("Switch {} flow stats received.")
+		conn.commit()
 
 	def _handle_TableStatsReceived(self, event):
+		conn = sql.connect(db)
+		c = conn.cursor()
 		sw = 's%s'%event.dpid
-		self.max_entries[sw] = event.stats[0].max_entries
-		self.tableActiveCount[sw] = event.stats[0].active_count
-		print "TableStatsReceived"
-		print self.tableActiveCount
-
-		self.matched_count
-		logger("Switch {} has received {} bytes and transmitted {} bytes.".format(event.dpid, self.max_entries, self.active_count, self.matched_count))
+		for f in event.stats:
+			self.max_entries[sw] = f.max_entries
+			self.tableActiveCount[sw] = f.active_count
+			logger("TableStatsReceived %s"%self.tableActiveCount)
+			c.execute('INSERT INTO "{}" (table_id, name, wildcards, max_entries, active_count, lookup_count, matched_count) VALUEs (%s, %s, %s, %s, %s, %s, %s)'.format(tables.flowtables), (f.table_id, f.name, f.wildcards, f.max_entries, f.active_count, f.lookup_count, f.matched_count))
+			logger("Switch {} flow table {} with max entries {}, {} active flows and {} matched.".format(sw, f.table_id if not f.name else f.name, f.max_entries, f.active_count, f.matched_count))
+		conn.commit()
 
 	def _handle_QueueStatsReceived(self, event):
+		conn = sql.connect(db)
+		c = conn.cursor()
+		sw = 's%s'%event.dpid
 		for f in event.stats:
 			self.transmitted = f.tx_bytes
 			self.packets = f.tx_packets
 			self.errors = f.tx_errors
-		logger("Switch {} has received {} bytes and transmitted {} bytes.".format(event.dpid, self.transmitted, self.packets, self.errors))
-
-	def _handle_PortStatsReceived(self, event):
-		for f in event.stats:
-			self.received = self.received - f.rx_bytes
-			self.transmitted = self.transmitted - f.tx_bytes
-		logger("Switch {} has received {} bytes and transmitted {} bytes.".format(event.dpid, self.received, self.transmitted))
+			c.execute('INSERT INTO "{}" (port_no, queue_id, tx_bytes, tx_packets, tx_errors) VALUES (%s, %s, %s, %s, %s)'format(tables.queues), (f.port_no, f.queue_id, f.tx_bytes, f.tx_packets, f.tx_errors))
+			logger("Switch {} queue stats {} bytes transmitted, {} packets transmitted and {} errors.".format(sw, self.transmitted, self.packets, self.errors))
+		conn.commit()
 
 	def handleStats(self):
 		rx = self.received * 8 / self.interval
